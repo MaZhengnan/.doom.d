4774b69bb7edd70bf72316aa6e4c6c00db7ee0bb
mzn <mzn836443465@163.com>
2025-07-16 19:44:53 +0800

    cmake work stream completed

--- config.el
+++ config.el
@@ -5,6 +5,12 @@
 (load! "+lang-python")
 (load! "+cmake-cpp")
 
+
+;; 快速 ESC 配置（jk/kj/jj）
+(setq evil-escape-key-sequence "jk"
+      evil-escape-delay 0.3   ; 按键间最大间隔秒数
+      evil-escape-excluded-major-modes '(vterm-mode eshell-mode term-mode))
+(evil-escape-mode 1)
 ;; ======================
 ;; Tree-sitter Highlighter
 ;; ======================
